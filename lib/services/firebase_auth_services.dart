b174b4dc820423c676b11b480857dea8f814ff7d
georgetacanisag <98815854+georgetacanisag@users.noreply.github.com>
2022-08-23 16:58:14 +0200

    update email and password services

--- lib/services/firebase_auth_services.dart
+++ lib/services/firebase_auth_services.dart
@@ -14,29 +14,32 @@ class FirebaseAuthServices {
   }
 
   //Sign in using email and password
-  Future<void> signInWithEmailAndPassword({required String email, required password}) async {
+  Future<void> signInWithEmailAndPassword(
+      {required String email, required password}) async {
     try {
-        await _auth.signInWithEmailAndPassword(email: email, password: password)
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
     } catch (e) {
       log("$e");
     }
   }
 
   //User registration with email and password
-  Future<void> resgisterWithEmailAndPassword ({required String email, required password}) async {
-    try{
-        await _auth.createUserWithEmailAndPassword(email: email, password: password)
-    } on FirebaseAuthException catch (e){
-        log("$e");
+  Future<void> resgisterWithEmailAndPassword(
+      {required String email, required password}) async {
+    try {
+      await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      log("$e");
     }
   }
 
   //Password reset email
   Future<void> sendPasswordResetEmail({required String email}) async {
-    try{
-        await _auth.sendPasswordResetEmail(email: email);
-    }catch(e){
-        log("$e");
+    try {
+      await _auth.sendPasswordResetEmail(email: email);
+    } catch (e) {
+      log("$e");
     }
-  }  
+  }
 }
