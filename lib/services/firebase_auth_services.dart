91a8986cf9171ec5e0e3be54086e34dc6d74c9ad
georgetacanisag <98815854+georgetacanisag@users.noreply.github.com>
2022-08-23 16:57:23 +0200

    create email and password services

---
+++ lib/services/firebase_auth_services.dart
@@ -0,0 +1,42 @@
+import 'dart:developer';
+
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FirebaseAuthServices {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //Firebase user a realtime stream
+  Stream<User?> get user => _auth.authStateChanges();
+
+  //Sign out
+  Future<void> signOut() {
+    return _auth.signOut();
+  }
+
+  //Sign in using email and password
+  Future<void> signInWithEmailAndPassword({required String email, required password}) async {
+    try {
+        await _auth.signInWithEmailAndPassword(email: email, password: password)
+    } catch (e) {
+      log("$e");
+    }
+  }
+
+  //User registration with email and password
+  Future<void> resgisterWithEmailAndPassword ({required String email, required password}) async {
+    try{
+        await _auth.createUserWithEmailAndPassword(email: email, password: password)
+    } on FirebaseAuthException catch (e){
+        log("$e");
+    }
+  }
+
+  //Password reset email
+  Future<void> sendPasswordResetEmail({required String email}) async {
+    try{
+        await _auth.sendPasswordResetEmail(email: email);
+    }catch(e){
+        log("$e");
+    }
+  }  
+}
